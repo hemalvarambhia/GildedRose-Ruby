fdfdb9910424d059ffac6f2dc12e652820f68ef7
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-11-21 17:46:44 +0000

    add specs for Aged Brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -3,3 +3,42 @@ describe '2 + 2' do
     expect(2 + 2).to eq 4
   end
 end
+require 'gilded_rose'
+
+describe GildedRose do
+  describe 'Aged Brie' do
+    let(:gilded_rose) { GildedRose.new([aged_brie]) }
+
+    context 'when it is not passed its sell by date' do
+      let(:aged_brie) { Item.new('Aged Brie', 20, 1) }
+      
+      it 'increases in quality' do
+        expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 1
+      end
+    end
+
+    context 'when it hits the sell by date' do
+      let(:aged_brie) { Item.new('Aged Brie', 0, 1) }
+
+      it 'increases in quality twice as fast' do
+        expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
+      end
+    end
+
+    context 'when it is passed its sell by date' do
+      let(:aged_brie) { Item.new('Aged Brie', -1, 1) }
+      
+      it 'increases in quality twice as fast' do
+        expect { gilded_rose.update_quality }.to change { aged_brie.quality }.by 2
+      end
+    end
+
+    context 'given the quality is 50' do
+      let(:aged_brie) { Item.new('Aged Brie', 2, 50) }
+      
+      it 'does not change the quality' do
+        expect { gilded_rose.update_quality }.not_to change { aged_brie.quality }
+      end
+    end
+  end
+end
