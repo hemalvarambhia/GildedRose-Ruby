44ec634fb990df289b2e1aefa3ae6a06e6ac6486
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-11-23 13:15:23 +0000

    Given a normal item when the quality is already 0 when the GildedRose updates its quality the quality does not change from 0.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -74,6 +74,22 @@ describe GildedRose do
       it 'reduces the number of days left to sell it' do
         expect { gilded_rose.update_quality }.to change { normal_item.sell_in }.by -1
       end
+
+      context 'when the quality is 0' do
+        let(:normal_item) { Item.new('Normal Item', 20, 0) }
+        
+        it "doesn't change from 0" do
+          expect { gilded_rose.update_quality }
+            .not_to change { normal_item.quality }.from 0
+        end
+      end
+    end
+
+    context 'when the hits its sell by date' do
+      let(:normal_item) { Item.new('Normal Item', 0, 10) }
+      it 'degrades the quality twice as fast' do
+        expect { gilded_rose.update_quality }.to change { normal_item.quality }. by -2
+      end
     end
   end
 end
