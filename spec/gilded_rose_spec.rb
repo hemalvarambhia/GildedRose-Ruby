b924777731f5d40e6b95714ffc93dc7641972502
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-11-30 13:12:54 +0000

    add specs for backstage passes behaviour

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -60,6 +60,69 @@ describe GildedRose do
     end
   end
 
+  describe 'Backstage passes to a TAFKAL80ETC concert' do
+    let(:gilded_rose) { GildedRose.new([backstage_pass]) }
+    let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 10) }
+
+    it 'reduces the number of days left to sell it' do
+      expect { gilded_rose.update_quality }.to change { backstage_pass.sell_in }.by -1
+    end
+
+    context 'when there are more than 10 days until the concert' do
+      it 'increases in quality by 1' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 1
+      end
+    end
+
+    context 'when there exactly 10 days left until the concert' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10) }
+
+      it 'increases the quality by 2' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 2
+      end
+    end
+
+    context 'when there are between 10 and 6 days left until the concert' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 8, 10) }
+
+      it 'increases the quality by 2' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 2
+      end
+    end
+
+    context 'when there are 5 days left until the concert' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 10) }
+
+      it 'increases the quality by 2' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 3
+      end
+    end
+
+    context 'when there are 5 days or less left until the concert' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 3, 10) }
+
+      it 'increases the quality by 2' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.by 3
+      end
+    end
+
+    context 'on the day of the concert' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 10) }
+
+      it 'reduces the quality to 0' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.to 0
+      end
+    end
+
+    context 'after the concert has passed' do
+      let(:backstage_pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', -2, 10) }
+
+      it 'is worth nothing' do
+        expect { gilded_rose.update_quality }.to change { backstage_pass.quality }.to 0
+      end
+    end
+  end
+
   describe 'Normal Item' do
     let(:gilded_rose) { GildedRose.new([normal_item]) }
     
