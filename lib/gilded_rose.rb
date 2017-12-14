67109379e74ebb470e8b06eab0c87e6af0f4ffa8
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-14 13:24:44 +0000

    Refactor - removed duplicate code to an intent-revealing method.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,7 +9,7 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
@@ -17,14 +17,10 @@ class GildedRose
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality_of(item)
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              increase_quality_of(item)
             end
           end
         end
@@ -37,18 +33,22 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+          increase_quality_of(item)
         end
       end
+    end
+  end
+
+  def increase_quality_of(item)
+    if item.quality < 50
+      item.quality += 1
     end
   end
 end
