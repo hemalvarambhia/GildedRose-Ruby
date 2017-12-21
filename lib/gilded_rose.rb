829edc99594785dfeea1e630db96f3ac53908957
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-21 10:55:41 +0000

    Inverted the logic so that the ifs use positive clause.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,13 +6,7 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -= 1
-          end
-        end
-      else
+      if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert"
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
@@ -22,6 +16,12 @@ class GildedRose
             if item.sell_in < 6
               increase_quality_of(item)
             end
+          end
+        end
+      else
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality -= 1
           end
         end
       end
