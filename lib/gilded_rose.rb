340b24e5fef7685a1e18f6b2e2a2645bb4137d08
Hemal Varambhia <h.n.varambhia@gmail.com>
2017-12-28 10:13:44 +0000

    Extracted the sell by date change logic to an intent-revealing method, moved the sulfuras logic into its own when branch.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,9 +6,9 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      next if item.name == "Sulfuras, Hand of Ragnaros"
-      item.sell_in = item.sell_in - 1
       case item.name
+      when 'Sulfuras, Hand of Ragnaros'
+        # NO OP
       when 'Backstage passes to a TAFKAL80ETC concert'
         update_backstage_passes(item)
       when "Aged Brie"
@@ -31,11 +31,16 @@ class GildedRose
     end
   end
 
+  def update_sell_by_date(item)
+    item.sell_in = item.sell_in - 1
+  end
+
   def reduce_quality_of(item)
     item.quality -= 1 if item.quality > 0
   end
 
   def update_backstage_passes(item)
+    update_sell_by_date(item)
     increase_quality_of(item)
     increase_quality_of(item) if item.sell_in < 11
     increase_quality_of(item) if item.sell_in < 6
@@ -43,11 +48,13 @@ class GildedRose
   end
 
   def update_aged_brie(item)
+    update_sell_by_date(item)
     increase_quality_of(item)
     increase_quality_of(item) if expired?(item)
   end
 
   def update_normal_item(item)
+    update_sell_by_date(item)
     reduce_quality_of(item)
     reduce_quality_of(item) if expired?(item)
   end
